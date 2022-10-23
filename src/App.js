c6deb8b812b8b59cf6ae43b20709b8b98015b09b
iamhosto10 <gerav0400@gmail.com>
2022-10-23 11:30:38 -0500

    Rest API

--- src/App.js
+++ src/App.js
@@ -1,95 +1,176 @@
 import arrow from "./images/icon-arrow.svg";
 import background from "./images/pattern-bg.png";
-import {MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
 import icon from "./icon";
+import { Section } from "./cover/cover.elements";
 
 // import { map } from "leaflet";
 
 //https://geo.ipify.org/api/v2/country?apiKey=at_hNsejjtlZB9giOoN9kFFhE26FvUSY&ipAddress=8.8.8.8
 
 function App() {
-  const [position,setPosition] = useState([37.38605,-122.08385]);
-  const [address,setAddress] = useState(null);
+  const [position, setPosition] = useState([37.38605, -122.08385]);
+  const [address, setAddress] = useState(null);
+  const [search, setSearch] = useState("");
   // const [ipAddress,setipAddress] = useState("");
   // const map = useMap();
 
   useEffect(() => {
-    try{
-      const getinitalData = async () =>{
-        const res  = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_API_KEY}&ipAddress=8.8.8.8`);//181.52.252.176`);
-        const data = await res.json()
+    try {
+      const getinitalData = async () => {
+        const res = await fetch(
+          `https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_API_KEY}&ipAddress=8.8.8.8`
+        ); //181.52.252.176`);
+        const data = await res.json();
         setAddress(data);
-        setPosition([data.location.lat,data.location.lng]);
-        await console.log(data,"hola");
-      }
+        setPosition([data.location.lat, data.location.lng]);
+        await console.log(data, "hola");
+      };
       getinitalData();
-    }catch(e){
+    } catch (e) {
       console.log(e);
     }
-  }, [])
+  }, []);
 
-  
+  const searchAPI = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_API_KEY}&ipAddress=${search}`
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setAddress(data);
+      } else {
+        window.alert("IP Address no valid please try again");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <>
-    <div className="absolute">
-      <img src={background} alt="" className="w-full h-80 object-cover"/>
-    </div>
-    <section className="relative pt-8">
-      <article>
-        <h1 className="text-2xl text-center text-white font-bold "> IP Address Tracker</h1>
-        <form className="mt-8 flex justify-center max-w-md mx-auto">
-          <input type="text" name= "ipaddress" id = "ipaddress" className="pt-6 pb-5 px-5 rounded-l-lg w-full"  placeholder="Search for any IP Address or domain" required/>
-          <button onClick={()=> setPosition(position)} type="submit" className="py-7 px-5 rounded-r-lg bg-black hover:opacity-80"> <img src ={arrow} alt ="arrow" className="h-4/5"  /></button> 
-        </form>
-      </article>
-      <article className="bg-white rounded-lg shadow p-8 mt-8 mx-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4 max-w-6xl lg:mx-auto lg:-mb-16 md:-mb-24 -mb-60 relative" style={{zIndex:100000,}}>
-        <div className="lg:border-r lg:border-slate-400">
-          <h2 className="uppercase text-sm font-bold tracking-wider text-center md:mb-2 md:text-left " style={{color:'hsl(0, 0%, 59%)',}}>
-            IP Address
-          </h2>
-          <p className="font-semibold text-lg md:text-xl xl:text-2xl text-center md:text-left" style={{color:'hsl(0, 0%, 17%)',}}>
-            {address && address.ip}
-          </p>
-        </div>
-        <div className="lg:border-r lg:border-slate-400">
-          <h2 className="uppercase text-sm font-bold tracking-wider text-center md:mb-2 md:text-left " style={{color:'hsl(0, 0%, 59%)',}}>
-            Location
-          </h2>
-          <p className="font-semibold text-lg md:text-xl xl:text-2xl text-center md:text-left" style={{color:'hsl(0, 0%, 17%)',}}>
-          {address && address.location.city},{address && address.location.region}
-          </p>
-        </div>
-        <div className="lg:border-r lg:border-slate-400">
-          <h2 className="uppercase text-sm font-bold tracking-wider text-center md:mb-2 md:text-left " style={{color:'hsl(0, 0%, 59%)',}}>
-            Timezone
-          </h2>
-          <p className="font-semibold text-lg md:text-xl xl:text-2xl text-center md:text-left" style={{color:'hsl(0, 0%, 17%)',}}>
-            UTC {address && address.location.timezone}
-          </p>
-        </div>
-        <div >
-          <h2 className="uppercase text-sm font-bold tracking-wider text-center md:mb-2 md:text-left " style={{color:'hsl(0, 0%, 59%)',}}>
-            ISP
-          </h2>
-          <p className="font-semibold text-lg md:text-xl xl:text-2xl text-center md:text-left" style={{color:'hsl(0, 0%, 17%)',}}>
-          {address && address.isp}
-          </p>
-        </div>
-      </article>
-      <MapContainer center={position} zoom={13} scrollWheelZoom={true} style ={{height : "495px", width: "100vw",}}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position} icon={icon}>
+      <div className="absolute">
+        <img src={background} alt="" className="w-full h-80 object-cover" />
+      </div>
+      <Section>
+        {/* //className="relative pt-8" */}
+        <article>
+          <h1>
+            {/* //className="text-2xl text-center text-white font-bold " */}
+            IP Address Tracker
+          </h1>
+          <form onSubmit={searchAPI}>
+            {/* className="mt-8 flex justify-center max-w-md mx-auto" */}
+            <input
+              type="text"
+              name="ipaddress"
+              id="ipaddress"
+              onChange={(e) => setSearch(e.target.value)}
+              value={search}
+              // className="pt-6 pb-5 px-5 rounded-l-lg w-full"
+              placeholder="Search for any IP Address or domain"
+              required
+            />
+            <button
+              // onClick={() => setPosition(position)}
+              type="submit"
+              // className="py-7 px-5 rounded-r-lg bg-black hover:opacity-80"
+            >
+              {" "}
+              <img
+                src={arrow}
+                alt="arrow"
+                // className="h-4/5"
+              />
+            </button>
+          </form>
+        </article>
+        <article
+          className="informacion"
+          //  bg-white rounded-lg shadow p-8 mt-8 mx-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4 max-w-6xl lg:mx-auto lg:-mb-16 md:-mb-24 -mb-60 relative
+          // style={{ zIndex: 100000 }}
+        >
+          <div className="bord ">
+            {/* lg:border-r lg:border-slate-400 */}
+            <h2
+            // className="uppercase text-sm font-bold tracking-wider text-center md:mb-2 md:text-left "
+            // style={{ color: "hsl(0, 0%, 59%)" }}
+            >
+              IP Address
+            </h2>
+            <p
+            // className="font-semibold text-lg md:text-xl xl:text-2xl text-center md:text-left"
+            // style={{ color: "hsl(0, 0%, 17%)" }}
+            >
+              {address && address.ip}
+            </p>
+          </div>
+          <div className="bord ">
+            {/* lg:border-r lg:border-slate-400 */}
+            <h2
+            // className="uppercase text-sm font-bold tracking-wider text-center md:mb-2 md:text-left "
+            // style={{ color: "hsl(0, 0%, 59%)" }}
+            >
+              Location
+            </h2>
+            <p
+            // className="font-semibold text-lg md:text-xl xl:text-2xl text-center md:text-left"
+            // style={{ color: "hsl(0, 0%, 17%)" }}
+            >
+              {address && address.location.city},
+              {address && address.location.region}
+            </p>
+          </div>
+          <div className="bord ">
+            {/* lg:border-r lg:border-slate-400 */}
+            <h2
+            // className="uppercase text-sm font-bold tracking-wider text-center md:mb-2 md:text-left "
+            // style={{ color: "hsl(0, 0%, 59%)" }}
+            >
+              Timezone
+            </h2>
+            <p
+            // className="font-semibold text-lg md:text-xl xl:text-2xl text-center md:text-left"
+            // style={{ color: "hsl(0, 0%, 17%)" }}
+            >
+              UTC {address && address.location.timezone}
+            </p>
+          </div>
+          <div>
+            <h2
+            // className="uppercase text-sm font-bold tracking-wider text-center md:mb-2 md:text-left "
+            // style={{ color: "hsl(0, 0%, 59%)" }}
+            >
+              ISP
+            </h2>
+            <p
+              className="font-semibold text-lg md:text-xl xl:text-2xl text-center md:text-left"
+              style={{ color: "hsl(0, 0%, 17%)" }}
+            >
+              {address && address.isp}
+            </p>
+          </div>
+        </article>
+        <MapContainer
+          center={position}
+          zoom={13}
+          scrollWheelZoom={true}
+          style={{ height: "495px", width: "100vw" }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position} icon={icon}>
             <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+              A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
-        </Marker>
-      </MapContainer>
-    </section>
+          </Marker>
+        </MapContainer>
+      </Section>
     </>
   );
 }
---
+++ src/cover/cover.elements.js
@@ -0,0 +1,107 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+  position: relative;
+  padding-top: 2rem;
+  h1 {
+    font-size: 1.5rem;
+    text-align: center;
+    color: white;
+    font-weight: 700;
+  }
+  h2 {
+    color: hsl(0, 0%, 59%);
+    text-transform: uppercase;
+    font-size: 0.875rem /* 14px */;
+    line-height: 1.25rem /* 20px */;
+    font-weight: 700;
+    letter-spacing: 0.05em;
+    text-align: center;
+  }
+  p {
+    color: hsl(0, 0%, 17%);
+    font-weight: 600;
+    font-size: 1.125rem /* 18px */;
+    line-height: 1.75rem /* 28px */;
+    text-align: center;
+  }
+  form {
+    margin-top: 2rem;
+    display: flex;
+    justify-content: center;
+    max-width: 28rem;
+    margin: 0 auto;
+  }
+  input {
+    padding: 1.5rem 1.25rem 1.25rem;
+    border-top-left-radius: 0.5rem /* 8px */;
+    border-bottom-left-radius: 0.5rem /* 8px */;
+    width: 100%;
+  }
+  button {
+    padding: 1.75rem 1.25rem;
+    border-top-right-radius: 0.5rem /* 8px */;
+    border-bottom-right-radius: 0.5rem /* 8px */;
+    background-color: black;
+    &.hover\:opacity-80:hover {
+      opacity: 0.8;
+    }
+    img {
+      height: 80%;
+    }
+  }
+  .informacion {
+    position: relative;
+    z-index: 100000;
+    background-color: white;
+    border-radius: 0.5rem /* 8px */;
+    --tw-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
+    --tw-shadow-colored: 0 1px 3px 0 var(--tw-shadow-color),
+      0 1px 2px -1px var(--tw-shadow-color);
+    box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000),
+      var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+    padding: 2rem /* 32px */;
+    margin-top: 2rem /* 32px */;
+    margin-left: 2rem /* 32px */;
+    margin-right: 2rem /* 32px */;
+    display: grid;
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+    gap: 2rem /* 32px */;
+    max-width: 72rem /* 1152px */;
+    margin-bottom: -15rem /* -240px */;
+  }
+
+  @media (min-width: 768px) {
+    h2 {
+      margin-bottom: 0.5rem /* 8px */;
+      text-align: left;
+    }
+    p {
+      font-size: 1.25rem /* 20px */;
+      line-height: 1.75rem /* 28px */;
+      text-align: left;
+      word-wrap: break-word;
+      /* hyphens: auto; */
+    }
+    .informacion {
+      grid-template-columns: repeat(2, minmax(0, 1fr));
+      margin-bottom: -6rem /* -96px */;
+    }
+  }
+  @media (min-width: 1024px) {
+    p {
+      font-size: 1.5rem /* 24px */;
+      line-height: 2rem /* 32px */;
+    }
+    .informacion {
+      grid-template-columns: repeat(4, minmax(0, 1fr));
+      margin: 2rem auto -15rem;
+      margin-bottom: -4rem /* -64px */;
+    }
+    .bord {
+      border-right-width: 1px;
+      --tw-border-opacity: 1;
+      border-color: rgb(148 163 184 / var(--tw-border-opacity));
+    }
+  }
+`;
